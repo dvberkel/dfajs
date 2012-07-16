ddbce4f0b70626562950e4b1e3b79b3c5b3ca943
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-16 14:57:41 +0200

    Namespace added
    A namespace for the project is added.

---
+++ spec/NamespaceSpec.js
@@ -0,0 +1,5 @@
+describe("FA", function(){
+    it("should be a namespace", function(){
+	expect(FA).toBeDefined();
+    });
+});
---
+++ src/NameSpace.js
@@ -0,0 +1,5 @@
+(function(undefined){
+    window.FA = {
+	version : "1.0.0"
+    };
+})();
