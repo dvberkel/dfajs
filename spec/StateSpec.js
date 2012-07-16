4a0024e86b98159c6e13ddcfd6be43837cba34e2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-16 15:27:44 +0200

    Accept method
    The accept method is introduced on the State object.

--- spec/StateSpec.js
+++ spec/StateSpec.js
@@ -2,4 +2,14 @@ describe("a State", function(){
     it("should be defined", function(){
 	expect(FA.State).toBeDefined();
     });
+
+    it("should default not accept any string", function(){
+	var state = new FA.State();
+	
+	console.log(state);
+
+	expect(state.accept("")).toBe(false);
+	expect(state.accept("A")).toBe(false);
+	expect(state.accept("B")).toBe(false);
+    });
 });
--- src/State.js
+++ src/State.js
@@ -1,5 +1,9 @@
 (function(Backbone, FA, undefined){
-    var State = Backbone.Model.extend();
+    var State = Backbone.Model.extend({
+	accept : function(target){
+	    return false;
+	}
+    });
 
     FA.State = State;    
 })(Backbone, FA);
